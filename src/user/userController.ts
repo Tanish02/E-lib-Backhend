29db479c6d91ec0a0f106a8e38995d5aa9b5087e
Tanish <tanishsharma080@gmail.com>
2025-04-06 03:47:33 +0530

    add login end point with circular dependency error carrying forword

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,22 +1,28 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
-import userModel from "./userModel";
+import  userModel  from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../Config/config"; 
 import { User } from "./userTypes";
+
+
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { name, email, password } = req.body;
 
     // Validation
+
+
     if (!name || !email || !password) {
       const error = createHttpError(400, "All fields are required");
       return next(error);
     }
 
     // Database call.
+
+
     try {
       const existingUser = await userModel.findOne({ email });
 
@@ -50,11 +56,13 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     }
 
     try {
+
+
       // Token generation JWT
-      const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
-        expiresIn: "7d",
-        algorithm: "HS256",
-      });
+         const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+          expiresIn: "7d",
+          algorithm: "HS256",
+        });   
 
       // Response
 
@@ -70,9 +78,47 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
      
 
 
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(createHttpError(400,
+       "All fields are required"));
+  }
+
+
+  try{
+  const user = await userModel.findOne({ email });
+  if (!user){
+    return next(createHttpError(404,
+       "user not found"));
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return next(createHttpError(400,
+       "Username or password incorrect!"));
+  }
+
+  // create access token
+  // to-do try catch error handling
+  const token = sign({ sub: user._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+    algorithm: "HS256",
+  });
+
+
+  res.json({ accessToken: token });
+} catch (err) {
+  return next(createHttpError(500,
+      "Error while getting user"));
+  }
+
+  
+
 
    
 
 // end code 
 
-export { createUser };
+export { createUser, loginUser };
--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import { createUser } from './userController';
 import { RequestHandler } from 'express';
+import { loginUser } from './userController';
 
 const userRouter = express.Router();
 
 //routes
 // userRouter.post("/register", createUser as RequestHandler);
 
-userRouter.post("/register", createUser);
+    userRouter.post("/register", createUser);
+    userRouter.post("/login", loginUser);
 
 
 
